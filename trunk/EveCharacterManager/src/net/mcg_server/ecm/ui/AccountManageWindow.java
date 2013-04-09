7a0be1224214f1f2cab64fe6571d568fce0c57f8
eriknpeters@gmail.com <eriknpeters@gmail.com@f8c145ee-751d-6313-a003-070c73cffb7d>
2013-04-09 02:36:37 +0000

    Changed AccountManageWindow - shows page description and includes an "Add Account" button that launches the AddAccountWindow page. Changed AddAccountWindow - added 2 labels and 2 jtextfields, where a user enters the EVE API KeyID and vCode values. Removed jeveassets.jar from Build Path and /lib directory

--- trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AccountManageWindow.java
+++ trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AccountManageWindow.java
@@ -1,14 +1,22 @@
 package net.mcg_server.ecm.ui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 public class AccountManageWindow {
 	
 	JFrame frame;
+	
+	final static boolean shouldFill = true;
+	final static boolean shouldWeightX = true;
+	final static boolean RIGHT_TO_LEFT = false;
 	
 	public AccountManageWindow(){
 		initialize();
@@ -17,25 +25,42 @@ public class AccountManageWindow {
 
 	private void initialize(){
 		frame = new JFrame("Account Management");
-		frame.setBounds(100,100,400,300);
-		JPanel panel = new JPanel();
+		frame.setBounds(100,100,400,150);
+		
+		JPanel panel = new JPanel(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		if(shouldFill){
+			//natural height, max width
+			c.fill = GridBagConstraints.HORIZONTAL;
+		}
 		
 		frame.add(panel);
 		
 		JTextArea description = new JTextArea("This is where the page description goes \n"+
 					"More Text here using \\n. \n");
-		panel.add(description);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 2;
+		c.ipady = 4;
+		c.weightx = 0.3;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(description, c);
 		
-		JLabel keyIdLabel = new JLabel("KeyID: ");
-		panel.add(keyIdLabel);
+		JButton button = new JButton("Add Account");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipady = 4;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 1;
+		panel.add(button, c);
 		
-		JTextField keyId = new JTextField();
-		panel.add(keyId);
-		
-		JLabel vCodeLabel = new JLabel("\n \nvCode: ");
-		panel.add(vCodeLabel);
-		
-		JTextField vCode = new JTextField();
-		panel.add(vCode);
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				// open AddAccountWindow
+				new AddAccountWindow();
+			}
+		});
 	}
 }
--- trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AddAccountWindow.java
+++ trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AddAccountWindow.java
@@ -1,7 +1,67 @@
 package net.mcg_server.ecm.ui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class AddAccountWindow {
-	
-	
+
+	JFrame frame;
+	final static boolean shouldFill = true;
+
+	public AddAccountWindow(){
+		initialize();
+		frame.setVisible(true);
+	}
+
+	private void initialize(){
+		frame = new JFrame("Add Account");
+		frame.setBounds(100,100,400,300);
+		JPanel panel = new JPanel(new GridBagLayout());
+
+		GridBagConstraints c = new GridBagConstraints();
+		if(shouldFill){
+			//natural height, max width
+			c.fill = GridBagConstraints.HORIZONTAL;
+		}
+
+		frame.add(panel);
+
+		JLabel keyIdLabel = new JLabel("KeyID: ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.3;
+		c.ipady = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(keyIdLabel, c);
+
+		JTextField keyId = new JTextField();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 1;
+		c.ipady = 1;
+		c.gridx = 1;
+		c.gridy = 1;
+		panel.add(keyId, c);
+
+		JLabel vCodeLabel = new JLabel("\n \nvCode: ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.3;
+		c.ipady = 1;
+		c.gridx = 0;
+		c.gridy = 2;
+		panel.add(vCodeLabel, c);
+
+		JTextField vCode = new JTextField();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.7;
+		c.ipady = 1;
+		c.gridx = 1;
+		c.gridy = 2;
+		panel.add(vCode, c);
+	}
 
 }
