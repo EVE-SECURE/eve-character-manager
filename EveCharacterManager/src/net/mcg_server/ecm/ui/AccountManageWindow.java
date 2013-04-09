5d81d5c66d522869af529d479ec8e20b25a8d93b
eriknpeters@gmail.com <eriknpeters@gmail.com@f8c145ee-751d-6313-a003-070c73cffb7d>
2013-04-09 19:41:26 +0000

    Added functionality: AccountManageWindow and AddAccountWindow cannot be opened again if they already are open.  Closing allows you to re-open the windows.

--- EveCharacterManager/src/net/mcg_server/ecm/ui/AccountManageWindow.java
+++ EveCharacterManager/src/net/mcg_server/ecm/ui/AccountManageWindow.java
@@ -4,20 +4,25 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+import net.mcg_server.ecm.ECM;
+
 public class AccountManageWindow {
-	
+
 	JFrame frame;
-	
+
 	final static boolean shouldFill = true;
 	final static boolean shouldWeightX = true;
 	final static boolean RIGHT_TO_LEFT = false;
-	
+	public static boolean addAcctOpen = false;
+
 	public AccountManageWindow(){
 		initialize();
 		frame.setVisible(true);
@@ -26,19 +31,19 @@ public class AccountManageWindow {
 	private void initialize(){
 		frame = new JFrame("Account Management");
 		frame.setBounds(100,100,400,150);
-		
+
 		JPanel panel = new JPanel(new GridBagLayout());
-		
+
 		GridBagConstraints c = new GridBagConstraints();
 		if(shouldFill){
 			//natural height, max width
 			c.fill = GridBagConstraints.HORIZONTAL;
 		}
-		
+
 		frame.add(panel);
-		
+
 		JTextArea description = new JTextArea("This is where the page description goes \n"+
-					"More Text here using \\n. \n");
+				"More Text here using \\n. \n");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridwidth = 2;
 		c.ipady = 4;
@@ -46,7 +51,7 @@ public class AccountManageWindow {
 		c.gridx = 0;
 		c.gridy = 0;
 		panel.add(description, c);
-		
+
 		JButton button = new JButton("Add Account");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.ipady = 4;
@@ -54,12 +59,23 @@ public class AccountManageWindow {
 		c.gridx = 1;
 		c.gridy = 1;
 		panel.add(button, c);
-		
+
 		button.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
 				// open AddAccountWindow
-				new AddAccountWindow();
+				if(!addAcctOpen){
+					addAcctOpen = true;
+					new AddAccountWindow();
+				}
+			}
+		});
+		
+		frame.addWindowListener(new WindowAdapter(){
+			@Override
+			public void windowClosing(WindowEvent e){
+				ECM.manageAcctOpen = false;
+				frame.dispose();
 			}
 		});
 	}
