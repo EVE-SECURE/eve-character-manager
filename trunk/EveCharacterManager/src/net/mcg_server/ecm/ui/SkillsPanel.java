2ce91f1856f64ab00b73f4d669790a3b09e4eaef
eriknpeters@gmail.com <eriknpeters@gmail.com@f8c145ee-751d-6313-a003-070c73cffb7d>
2013-05-14 19:55:22 +0000

    Created SkillsPanel class and moved all related elements out of ECM class into SkillsPanel class.

---
+++ trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/SkillsPanel.java
@@ -0,0 +1,15 @@
+package net.mcg_server.ecm.ui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class SkillsPanel extends JPanel{
+
+	public SkillsPanel(){
+
+		JLabel label1 = new JLabel("Skills Panel");
+		this.add(label1);
+	}
+	
+}
