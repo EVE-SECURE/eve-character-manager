6e5b97cf63061ade3733fd5e1a5cdf322a3373a6
eriknpeters@gmail.com <eriknpeters@gmail.com@f8c145ee-751d-6313-a003-070c73cffb7d>
2013-05-20 19:42:16 +0000

    Adjusted the AddAccountWindow class to render a better-looking layout

--- trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AddAccountWindow.java
+++ trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AddAccountWindow.java
@@ -40,7 +40,7 @@ public class AddAccountWindow {
 
 		//create the frame and set properties
 		frame = new JFrame("Add Account");
-		frame.setBounds(100,100,400,300);
+		frame.setBounds(100,100,500,100);
 		frame.setAlwaysOnTop(true);
 		frame.setResizable(false);
 
@@ -52,62 +52,55 @@ public class AddAccountWindow {
 		if(shouldFill){
 			//natural height, max width
 			c.fill = GridBagConstraints.HORIZONTAL;
+			//c.gridwidth = 5;
 		}
 
 		//add the panel to the frame
 		frame.add(panel);
 
+		/*
+		 
 		//create empty space
 		Box box1 = new Box(0);
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 1;
 		c.gridx = 0;
 		c.gridy = 0;
-		c.gridwidth = 5;
 		panel.add(box1, c);
-
+		
+		*/
+		
 		//create label and define its position
 		JLabel keyIdLabel = new JLabel("  KeyID: ");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.3;
-		c.ipady = 1;
-		c.gridx = 0;
-		c.gridy = 1;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.weightx = 0.2;
 		panel.add(keyIdLabel, c);
 
 		//create text field and define its position
 		JTextField keyId = new JTextField();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 1;
-		c.ipady = 1;
-		c.gridx = 1;
-		c.gridy = 1;
+		c.gridx = 3;
+		c.gridy = 0;
+		c.weightx = 0.8;
 		panel.add(keyId, c);
 
 		//create label and define its position
 		JLabel vCodeLabel = new JLabel("  vCode: ");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.3;
-		c.ipady = 1;
-		c.gridx = 0;
-		c.gridy = 2;
+		c.gridx = 1;
+		c.gridy = 1;
+		c.weightx = 0.2;
 		panel.add(vCodeLabel, c);
 
 		//create text field and define its position
 		JTextField vCode = new JTextField();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.7;
-		c.ipady = 1;
-		c.gridx = 1;
-		c.gridy = 2;
+		c.gridx = 3;
+		c.gridy = 1;
+		c.weightx = 0.8;
 		panel.add(vCode, c);
 
 		//create button and define its position
 		JButton addAcctButton = new JButton("Add Account");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridx = 1;
-		c.gridy = 3;
+		c.gridx = 3;
+		c.gridy = 2;
+		c.weightx = 0.3;
 		panel.add(addAcctButton, c);
 
 
