b960f4993396b46480373d0ac79999d8192cdd08
eriknpeters@gmail.com <eriknpeters@gmail.com@f8c145ee-751d-6313-a003-070c73cffb7d>
2013-05-20 20:49:56 +0000

    Removed unused import from AddAccountWindow class.

--- trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AddAccountWindow.java
+++ trunk/EveCharacterManager/src/net/mcg_server/ecm/ui/AddAccountWindow.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
